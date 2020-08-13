a5cddde8582ec137b6246f8f7acf0ee50541dd72
maverick107A <68321073+maverick107A@users.noreply.github.com>
2020-08-13 10:39:53 +0900

    Clean
    다날림

--- Hungry_Dragon/CopyResources.bat
+++
@@ -1,6 +0,0 @@
-copy		.\Engine\Resources\Code\*.h		.\Reference\Header\		
-copy		.\Engine\Resources\Bin\Resources.dll	.\Client\Bin\		
-copy		.\Engine\Resources\Bin\Resources.lib	.\Reference\Library\	
-
-copy		.\Engine\Header\*.h			.\Reference\Header\
-copy		.\Engine\Header\*.inl			.\Reference\Header\
